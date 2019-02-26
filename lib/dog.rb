9b8991c22411e11145e8a12d9f2fcf8860839c3a
Jenny Chu <jchudesign0930@gmail.com>
2019-02-26 05:43:05 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  def initialize
+    dog_one
+    dog_two
+    dog_three
+  end
+  
+  def fido
+    puts "fido"
+  end
+  
+  def snoopy
+    puts "snoopy"
+  end
+  
+  def lassie
+    puts "lassie"
+  end
+  
+end
+
